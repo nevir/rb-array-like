b78862084b6c32006e037ad12e60488a952c7423
Ian MacLeod <ian@nevir.net>
2013-01-13 14:03:22 -0800

    perf spec

--- spec/unit/array_like/combinatorics/multiplication_operator_spec.rb
+++ spec/unit/array_like/combinatorics/multiplication_operator_spec.rb
@@ -9,4 +9,9 @@ describe ArrayLike::Combinatorics, "#*" do
     (subject * 2).should eq([0, 1, 1, 2, 3, 5, 8, 13, 0, 1, 1, 2, 3, 5, 8, 13])
   end
 
+  it "Should not bother coercing the subject to_a if given a string" do
+    subject.should_not_receive(:to_a)
+    (subject * "::").should eq("0::1::1::2::3::5::8::13")
+  end
+
 end
